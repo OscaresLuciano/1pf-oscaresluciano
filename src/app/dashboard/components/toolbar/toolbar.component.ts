26212108473473b59f40b3f07668c88692b90d29
Luciano Oscares <oscaresluciano@outlook.com>
2024-04-07 18:53:20 -0300

    UPDATE, titulo

--- src/app/dashboard/components/toolbar/toolbar.component.ts
+++ src/app/dashboard/components/toolbar/toolbar.component.ts
@@ -6,7 +6,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: []
 })
 export class ToolbarComponent {
-  title = 'Proyecto Final - Entrega final'
+  title = 'Proyecto PACUI'
   @Output()
   toggleSidebar = new EventEmitter();
 
